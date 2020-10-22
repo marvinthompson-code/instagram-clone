b43edf1fe2bcae02a682c29d3325d4a580095202
Marvin <marvinthompson@pursuit.org>
2020-10-22 02:35:31 -0400

    added guest login buttons

--- frontend/src/features/Home/Home.js
+++ frontend/src/features/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Login from "../Login/Login";
+import "../../css/Home.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -8,6 +10,15 @@ const Home = () => {
         <div className="col">Some text</div>
         <div className="col">
           <Login />
+          <br></br>
+          <div className="jumbotron pitchJumbo">
+            <small id="emailHelp" className="form-text text-muted">
+              Not a member?
+              <NavLink exact to={"/signup"}>
+                Sign Up
+              </NavLink>
+            </small>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/features/Login/Login.js
+++ frontend/src/features/Login/Login.js
@@ -1,42 +1,43 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
-import Nav from "../../Nav";
+import { NavLink } from "react-router-dom";
+import "../../css/Login.css";
+
 const Login = () => {
   return (
-    <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-        <small id="emailHelp" class="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">
-        Submit
-      </button>
-      <small id="emailHelp" class="form-text text-muted">
-          Not a member?
-        </small>
-        <NavLink exact to={"/signup"}>
-            Sign Up
-        </NavLink>
-    </form>
+    <div className="jumbotron loginJumbo">
+      <h1 className="display-4 FakerGramHeader">FakerGram</h1>
+      <form>
+        <div className="form-group emailDiv">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control loginInput"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group passwordDiv">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control loginInput"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="buttonDiv">
+          <button type="submit" className="btn btn-primary submitButton">
+            Submit
+          </button>
+          <hr class="my-4"></hr>
+          <button type="submit" className="btn btn-primary submitButton">
+            Guest Login
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default Login
+export default Login;
