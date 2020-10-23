f43978d65c2765b0ee80acf51e7e73f3887883e9
Marvin <marvinthompson@pursuit.org>
2020-10-23 17:16:35 -0400

    login finished

--- frontend/src/features/Login/Login.js
+++ frontend/src/features/Login/Login.js
@@ -1,12 +1,31 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { login } from "../../util/firebaseFunctions";
+import { updateUser } from "../User/userSlice";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import "../../css/Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await login(email, password);
+      dispatch(updateUser(res.user));
+      history.push("/feed");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
   return (
     <div className="jumbotron loginJumbo">
       <h1 className="display-4 FakerGramHeader">FakerGram</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group emailDiv">
           <label for="exampleInputEmail1">Email address</label>
           <input
@@ -15,6 +34,8 @@ const Login = () => {
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="form-group passwordDiv">
@@ -24,6 +45,8 @@ const Login = () => {
             className="form-control loginInput"
             id="exampleInputPassword1"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="buttonDiv">
