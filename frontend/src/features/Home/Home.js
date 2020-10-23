45b944df3131ea768f040f80a40c7dd8a00b5435
Marvin <marvinthompson@pursuit.org>
2020-10-23 16:29:17 -0400

    carousel

--- frontend/src/features/Home/Home.js
+++ frontend/src/features/Home/Home.js
@@ -2,18 +2,40 @@ import React from "react";
 import Login from "../Login/Login";
 import "../../css/Home.css";
 import { NavLink } from "react-router-dom";
+import instagramSlide1 from "../../css/images/instaslide1.jpg"
+import instagramSlide2 from "../../css/images/instaslide2.jpg"
+import instagramSlide3 from "../../css/images/instaslide3.jpg"
+
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="container homeContainer">
       <div className="row">
-        <div className="col">Some text</div>
-        <div className="col">
+        <div className="col imageColumn">
+          <div
+            id="carouselExampleSlidesOnly"
+            className="carousel slide screenshotCarousel"
+            data-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img className="d-block w-100 screenshotCarouselImg" src={instagramSlide1} alt="First slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100 screenshotCarouselImg" src={instagramSlide2} alt="Second slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100 screenshotCarouselImg" src={instagramSlide3} alt="Third slide" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col loginColumn">
           <Login />
           <br></br>
           <div className="jumbotron pitchJumbo">
-            <small id="emailHelp" className="form-text text-muted">
-              Not a member?
+            <small id="emailHelp" className="form-text text-muted nam">
+              Don't have an account? <span></span>
               <NavLink exact to={"/signup"}>
                 Sign Up
               </NavLink>
