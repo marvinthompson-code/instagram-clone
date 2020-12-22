8417c3ecaed0cefd2c07343473b8c084b11f81f4
Marvin <marvinthompson@pursuit.org>
2020-12-21 20:05:12 -0500

    fixing signup

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import tokenReducer from "../features/User/tokenSlice";
 import userReducer from "../features/User/userSlice";
+import logger from "redux-logger";
+
 export default configureStore({
   reducer: {
     token: tokenReducer,
     user: userReducer,
   },
-  middleware: [...getDefaultMiddleware()],
+  middleware: [...getDefaultMiddleware(), logger],
 });
--- frontend/src/features/SignUp/SignUpForm.js
+++ frontend/src/features/SignUp/SignUpForm.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { apiURL } from "../../util/apiURL";
+import { signUp } from "../../util/firebaseFunctions";
+
 import "../../css/SignupForm.css";
 
 const SignUpForm = () => {
+  // states
+
+  // variables
+  const API = apiURL();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  // image Upload
+
+  // handle firebase upload
+
+  // handle submit
+
+  
   return (
     <div className="jumbotron signupJumbo">
       <h1 className="display-4 FakerGramHeader">FakerGram</h1>
@@ -47,12 +67,14 @@ const SignUpForm = () => {
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
+            placeholder="Enter a Bio..."
           ></textarea>
         </div>
-
-        <button type="submit" className="btn btn-primary submitButton">
-          Submit
-        </button>
+        <div className="buttonDiv">
+          <button type="submit" className="btn btn-primary submitButton">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
